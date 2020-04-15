45dd08d49c5d760b346ea58e87da1bbba44af9c9
Amiel Filarca <amielfilarca@gmail.com>
2020-04-15 20:51:01 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+function startCountdown() {
+  status = "running";
+  // Set the date we're counting down to
+  let currentDate = new Date();
+  console.log(`Current time: ${currentDate.toLocaleTimeString()}`);
+  let sessionDuration = document.querySelector(".session > span").innerHTML;
+  console.log(`Session duration: ${sessionDuration}`);
+  let countdownDate = new Date(
+    currentDate.setMinutes(currentDate.getMinutes() + parseInt(sessionDuration))
+  );
+  console.log(`Counting down to: ${countdownDate.toLocaleTimeString()}`);
+
+  // Update the count down every 1 second
+  let x = setInterval(function () {
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countdownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.querySelector(".display > h1").innerHTML = `${minutes}:${seconds}`;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.querySelector(".display > h1").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
+
+function pauseCountdown() {}
+
+function stopCountdown() {}
+
+function resetCountdown() {}
+
+let status = "stopped";
+
+function isRunning() {
+  return status == "running" ? true : false;
+}
+
+const start = document.querySelector(".start > button");
+start.addEventListener("click", () => {
+  if (!isRunning()) {
+    startCountdown();
+  }
+});
+const pause = document.querySelector(".pause > button");
+start.addEventListener("click", () => {
+  pauseCountdown();
+});
+const stop = document.querySelector(".stop > button");
+start.addEventListener("click", () => {
+  stopCountdown();
+});
+const reset = document.querySelector(".reset > button");
+start.addEventListener("click", () => {
+  resetCountdown();
+});
