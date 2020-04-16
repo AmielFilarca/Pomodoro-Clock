e4b643c1bbaa42a53deffcb3a391a5a31542b19a
Amiel Filarca <amielfilarca@gmail.com>
2020-04-16 21:34:56 +0800

    Added timer customization

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ let timerInterval = null;
 const sessionDuration = document.querySelector("#session-duration");
 const breakDuration = document.querySelector("#break-duration");
 const timerDisplay = document.querySelector("#timer-display");
+const startButton = document.querySelector("#start-button");
+const pauseButton = document.querySelector("#pause-button");
+const stopButton = document.querySelector("#stop-button");
+const resetButton = document.querySelector("#reset-button");
+const increaseSessionDurationButton = document.querySelector(
+  "#increase-session-duration"
+);
+const decreaseSessionDurationButton = document.querySelector(
+  "#decrease-session-duration"
+);
+const increaseBreakDurationButton = document.querySelector(
+  "#increase-break-duration"
+);
+const decreaseBreakDurationButton = document.querySelector(
+  "#decrease-break-duration"
+);
 
 function isRunning() {
   return timerStatus == "running" ? true : false;
@@ -16,8 +32,7 @@ function isStopped() {
   return timerStatus == "stopped" ? true : false;
 }
 
-const start = document.querySelector(".start > button");
-start.addEventListener("click", () => {
+startButton.addEventListener("click", () => {
   if (!isRunning()) {
     startTimer();
   }
@@ -78,11 +93,10 @@ function startTimer() {
       clearInterval(x);
       timerDisplay.textContent = "EXPIRED";
     }
-  }, 1000);
+  }, 1);
 }
 
-const pause = document.querySelector(".pause > button");
-pause.addEventListener("click", () => {
+pauseButton.addEventListener("click", () => {
   pauseTimer();
 });
 
@@ -96,8 +110,7 @@ function pauseTimer() {
   }
 }
 
-const stop = document.querySelector(".stop > button");
-stop.addEventListener("click", () => {
+stopButton.addEventListener("click", () => {
   stopTimer();
 });
 
@@ -112,8 +125,7 @@ function stopTimer() {
   }
 }
 
-const reset = document.querySelector(".reset > button");
-reset.addEventListener("click", () => {
+resetButton.addEventListener("click", () => {
   resetTimer();
 });
 
@@ -125,6 +137,33 @@ function resetTimer() {
   breakDuration.textContent = "5";
   timerDisplay.textContent = `${sessionDuration.textContent}:00`;
 }
+
+increaseSessionDurationButton.addEventListener("click", () => {
+  if (sessionDuration.textContent < 60) {
+    sessionDuration.textContent++;
+  }
+  if (!isRunning()) {
+    timerDisplay.textContent = `${sessionDuration.textContent}:00`;
+  }
+});
+decreaseSessionDurationButton.addEventListener("click", () => {
+  if (sessionDuration.textContent > 1) {
+    sessionDuration.textContent--;
+  }
+  if (!isRunning()) {
+    timerDisplay.textContent = `${sessionDuration.textContent}:00`;
+  }
+});
+increaseBreakDurationButton.addEventListener("click", () => {
+  if (breakDuration.textContent < 60) {
+    breakDuration.textContent++;
+  }
+});
+decreaseBreakDurationButton.addEventListener("click", () => {
+  if (breakDuration.textContent > 1) {
+    breakDuration.textContent--;
+  }
+});
 
 // * On page load
 timerDisplay.textContent = `${sessionDuration.textContent}:00`;
